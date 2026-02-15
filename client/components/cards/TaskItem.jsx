7173d9d37eddeea047eaf99c00bb77af1a2bb76f
= <medtube835@gmail.com>
2026-02-15 17:05:06 +0000

    Create a separate comp for each task item

---
+++ client/components/cards/TaskItem.jsx
@@ -0,0 +1,35 @@
+import { Circle, CircleCheck, Trash2 } from "lucide-react"
+
+const TaskItem = ({t})=>{
+    return <div
+                className="group flex justify-between items-center p-4 hover:bg-gray-50 transition-colors"
+            >
+        <div
+                                className="flex items-center gap-3 cursor-pointer flex-1"
+                                onClick={() => handleChangeTaskStatus(t)}
+                            >
+                                <button className="transition-transform active:scale-90">
+                                    {t.status === 'active' ? (
+                                        <Circle className="text-gray-300 group-hover:text-gray-400" size={22} />
+                                    ) : (
+                                        <CircleCheck className="text-white rounded-full bg-green-500" size={22} />
+                                    )}
+                                </button>
+                                <h4
+                                    className={`text-md transition-all duration-300 ${
+                                        t.status === 'completed'
+                                            ? 'line-through text-gray-400 italic'
+                                            : 'text-gray-700 font-medium'
+                                    }`}
+                                >
+                                    {t.title}
+                                </h4>
+                            </div>
+
+                            <button className="p-2 opacity-0 group-hover:opacity-100 transition-opacity hover:bg-red-50 rounded-full" onClick={()=> handleDeleteTask(t._id)}>
+                                <Trash2 size={18} className="text-red-400 hover:text-red-600" />
+                            </button>
+                        </div>
+}
+
+export default TaskItem
--- client/pages/Todos.jsx
+++ client/pages/Todos.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useRef, useState } from "react";
 import { styles } from "../lib/styles";
-import { Circle, CircleCheck, Plus, Trash2 } from "lucide-react";
+import {  Plus } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import { addTask, changeTaskStatus, deleteTask, getAllTasks } from "../store/features/tasks/taskSlice";
+import TaskItem from "../components/cards/TaskItem";
 
 const Todos = () => {
     const dispatch = useDispatch();
@@ -104,36 +105,7 @@ const Todos = () => {
                     </div>
                 ) : (
                     filteredTasks.map((t) => (
-                        <div
-                            key={t._id}
-                            className="group flex justify-between items-center p-4 hover:bg-gray-50 transition-colors"
-                        >
-                            <div
-                                className="flex items-center gap-3 cursor-pointer flex-1"
-                                onClick={() => handleChangeTaskStatus(t)}
-                            >
-                                <button className="transition-transform active:scale-90">
-                                    {t.status === 'active' ? (
-                                        <Circle className="text-gray-300 group-hover:text-gray-400" size={22} />
-                                    ) : (
-                                        <CircleCheck className="text-white rounded-full bg-green-500" size={22} />
-                                    )}
-                                </button>
-                                <h4
-                                    className={`text-md transition-all duration-300 ${
-                                        t.status === 'completed'
-                                            ? 'line-through text-gray-400 italic'
-                                            : 'text-gray-700 font-medium'
-                                    }`}
-                                >
-                                    {t.title}
-                                </h4>
-                            </div>
-
-                            <button className="p-2 opacity-0 group-hover:opacity-100 transition-opacity hover:bg-red-50 rounded-full" onClick={()=> handleDeleteTask(t._id)}>
-                                <Trash2 size={18} className="text-red-400 hover:text-red-600" />
-                            </button>
-                        </div>
+                        <TaskItem key={t._id} t={t}/>
                     ))
                 )}
             </div>
